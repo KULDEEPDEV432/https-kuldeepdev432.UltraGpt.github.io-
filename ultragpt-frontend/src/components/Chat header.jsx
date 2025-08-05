bc26a5b0bc707f9e817ae40d95abca274574057d
KULDEEPDEV432 <kp4488773@gmail.com>
2025-08-06 00:17:43 +0530

    Create Chat header.jsx

---
+++ ultragpt-frontend/src/components/Chat header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ChatHeader = () => {
+  return (
+    <div className="chat-header">
+      <h2>ultraGpt</h2>
+      <p className="status">Online</p>
+    </div>
+  );
+};
+
+export default ChatHeader;
