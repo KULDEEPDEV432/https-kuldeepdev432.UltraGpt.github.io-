681a14bcfa7f090d97dcc851e881e5e0d887cfb0
Kuldeep Prajapati <kuldeepprajapati432@gmail.com>
2025-08-11 07:01:52 +0530

    Build project and add .nojekyll

---
+++ ultragpt-frontend/vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  optimizeDeps: {
+    include: [
+      'i18next',
+      'react-i18next',
+      'i18next-browser-languagedetector',
+      'i18next-http-backend'
+    ]
+  }
+})
